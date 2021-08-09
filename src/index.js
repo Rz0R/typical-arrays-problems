bab0a48306bf7b7f883587837625b37f191d76a4
Rz0R <vpa05@mail.ru>
2021-08-09 22:01:30 +1000

    feat: add functions min max avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
-exports.min = function min (array) {
-  return 0;
-}
-
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.min = function min(array = []) {
+    if (array.length) {
+      return Math.min(...array);
+    }
+    return 0;
+  }
+  
+  exports.max = function max(array = []) {
+    if (array.length) {
+      return Math.max(...array, 0);
+    }
+    return 0;
+  }
+  
+  exports.avg = function avg(array = []) {
+    if (array.length) {
+      return array.reduce((sum, val) => sum + val, 0) / array.length;
+    }
+    return 0;
+  }
